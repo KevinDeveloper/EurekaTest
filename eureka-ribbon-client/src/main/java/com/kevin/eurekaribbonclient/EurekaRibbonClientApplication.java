50493c6d3f64f39648288301effd5e1506d51413
Kevin <Kevin_developer@163.com>
2018-05-25 15:58:47 +0800

    合入原因：ribbon 集成hystrix dashboard。 修改人：kevin

--- eureka-ribbon-client/src/main/java/com/kevin/eurekaribbonclient/EurekaRibbonClientApplication.java
+++ eureka-ribbon-client/src/main/java/com/kevin/eurekaribbonclient/EurekaRibbonClientApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+@EnableHystrixDashboard
 @EnableHystrix
 @EnableEurekaClient
 @SpringBootApplication
 public class EurekaRibbonClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaRibbonClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaRibbonClientApplication.class, args);
+    }
 }
